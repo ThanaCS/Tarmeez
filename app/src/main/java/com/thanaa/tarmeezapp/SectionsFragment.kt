fd2151b1a0145115c47c4afb5ea8a0a05c778364
sarah <althubiti.sarah1996@gmail.com>
2021-01-05 17:28:09 +0300

    Added Sections Part

--- app/src/main/java/com/thanaa/tarmeezapp/SectionsFragment.kt
+++ app/src/main/java/com/thanaa/tarmeezapp/SectionsFragment.kt
@@ -5,55 +5,106 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.thanaa.tarmeezapp.data.Planet
+import com.thanaa.tarmeezapp.data.Section
+import com.thanaa.tarmeezapp.databinding.FragmentHomeBinding
+import com.thanaa.tarmeezapp.databinding.FragmentSectionsBinding
+import com.thanaa.tarmeezapp.databinding.ItemRowBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SectionsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SectionsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentSectionsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var sections = ArrayList<Section>()
+    val sectionsList: MutableList<Section> = mutableListOf()
+    var adapter = SectionsAdapter(sections)
+
+    private val args by navArgs<SectionsFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sections, container, false)
+        _binding = FragmentSectionsBinding.inflate(inflater, container, false)
+
+        FirebaseDatabase.getInstance().reference
+            .child("Planet")
+            .child(args.planetId.toString())
+            .child("section")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val a = snapshot.children
+                    snapshot.children.forEach{
+                        val title = it.child("sectionTitle").value.toString()
+                        val section = Section(title,"")
+                        sectionsList.add(section)
+                        println("########################################################################### ${it.child("sectionTitle").value}")
+
+                    }
+                    adapter.setData(sectionsList)
+                }
+
+            })
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SectionsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SectionsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
+    inner class SectionsAdapter(private var sectionsList: List<Section>) :
+        RecyclerView.Adapter<SectionsAdapter.ViewHolder>() {
+
+        inner class ViewHolder(val itemRowBinding: ItemRowBinding) : RecyclerView.ViewHolder(itemRowBinding.root) {
+
+            fun bind(section: Section) {
+                itemRowBinding.sectionTitle.text = section.sectionTitle
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val binding : ItemRowBinding = ItemRowBinding.inflate(layoutInflater, parent, false)
+            return ViewHolder(binding)
+        }
+
+        override fun getItemCount(): Int = sectionsList.size
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val sectionItem = sectionsList[position]
+            holder.bind(sectionItem)
+
+            holder.itemView.setOnClickListener {
+                val action = SectionsFragmentDirections.actionSectionsFragmentToContentFragment(sectionItem.sectionTitle)
+                findNavController().navigate(action)
+            }
+
+        }
+
+        fun setData(sections: List<Section>){
+            this.sectionsList = sections
+            notifyDataSetChanged()
+        }
+
+    }
+
+
 }
