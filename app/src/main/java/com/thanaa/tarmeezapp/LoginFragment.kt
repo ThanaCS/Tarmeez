b9e29229eadf32c8e09487ebf71b89492e2d7e9e
sarah <althubiti.sarah1996@gmail.com>
2021-01-05 13:01:59 +0300

    Read From Firebase

--- app/src/main/java/com/thanaa/tarmeezapp/LoginFragment.kt
+++ app/src/main/java/com/thanaa/tarmeezapp/LoginFragment.kt
@@ -1,21 +1,13 @@
 package com.thanaa.tarmeezapp
 
 import android.app.AlertDialog
-import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import com.thanaa.tarmeezapp.databinding.FragmentLoginBinding
-
-class LoginFragment : Fragment() {
-    private var _binding: FragmentLoginBinding? = null
-    private val binding get() = _binding!!
 import android.widget.EditText
 import android.widget.TextView
-
 import androidx.navigation.Navigation
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
@@ -39,27 +31,15 @@ class LoginFragment : Fragment() {
     ): View{
         hideNavigation()
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        binding.login.setOnClickListener {
-            Toast.makeText(requireContext(), "Hi", Toast.LENGTH_LONG).show()
-        }
-        return binding.root
-    }
-    //hide navigation button
-    private fun hideNavigation() {
-        val bottomNavigationView = (activity as MainActivity).bottomNavigationView
-        val fab = (activity as MainActivity).fab
-        val bottomAppBar = (activity as MainActivity).bottomAppBar
-        bottomNavigationView.visibility = View.GONE
-        bottomAppBar.visibility = View.GONE
-        fab.visibility = View.GONE
-        emailEditText = _binding!!.userEmail
-        passwordEditText = _binding!!.userPassword
-        newAccountTextView = _binding!!.newAccount
+        emailEditText = binding.userEmail
+        passwordEditText = binding.userPassword
+        newAccountTextView = binding.newAccount
         progressDialog = CustomProgressDialog(requireContext())
         alertDialog = AlertDialog.Builder(requireContext()).create()
         alertDialog.setMessage(getString(R.string.error_message))
-        passwordEditText.setOnClickListener {
-        }
+//        newAccountTextView.setOnClickListener {
+//            Navigation.findNavController(_binding!!.root).navigate(R.id.LoginFragmentToRegisterFragment)
+//        }
         binding.login.setOnClickListener {
             if (validation()){
                 progressDialog.show()
@@ -74,7 +54,6 @@ class LoginFragment : Fragment() {
                         }else{
                             progressDialog.dismiss()
                             alertDialog.show()
-
                         }
                     }
                 })
@@ -91,7 +70,6 @@ class LoginFragment : Fragment() {
         bottomAppBar.visibility = View.GONE
         fab.visibility = View.GONE
     }
-
     private fun validation():Boolean{
         var result = true
         if(emailEditText.text.isBlank()){
