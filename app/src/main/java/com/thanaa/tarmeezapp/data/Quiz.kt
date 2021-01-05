dfacb58d08394b5b89f5258a5c4a8e539e64771b
Sahar Nollily <Sa7arnollily@gmail.com>
2021-01-05 10:13:18 +0300

    add to firebase

--- app/src/main/java/com/thanaa/tarmeezapp/data/Quiz.kt
+++ app/src/main/java/com/thanaa/tarmeezapp/data/Quiz.kt
@@ -1,6 +1,5 @@
 package com.thanaa.tarmeezapp.data
 
-data class Quiz(val quizId: Int,
-                val question: String,
+data class Quiz(val question: String,
                 val answer: String,
                 val options: String)
