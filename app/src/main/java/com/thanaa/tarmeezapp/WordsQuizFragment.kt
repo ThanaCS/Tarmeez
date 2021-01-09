8a2e8bc9e5ec260168062db203e29be18db2c400
Lamia Ewais <lamia5098@gmail.com>
2021-01-09 23:10:57 +0300

    modify the layout of quizzes

--- app/src/main/java/com/thanaa/tarmeezapp/WordsQuizFragment.kt
+++ app/src/main/java/com/thanaa/tarmeezapp/WordsQuizFragment.kt
@@ -1,6 +1,7 @@
 package com.thanaa.tarmeezapp
 
 import android.graphics.Color
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.Gravity
 import androidx.fragment.app.Fragment
@@ -9,27 +10,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.core.view.forEach
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.thanaa.tarmeezapp.databinding.FragmentDragAndDropQuizBinding
 import com.thanaa.tarmeezapp.databinding.FragmentWordsQuizBinding
+import org.jetbrains.anko.support.v4.toast
 import java.util.*
 
 class WordsQuizFragment : Fragment() {
 
     private val args by navArgs<WordsQuizFragmentArgs>()
     private lateinit var questionTextView: TextView
+    private lateinit var moveToSection:TextView
     private val listOfButton = mutableListOf<TextView>()
-
+    private var countNumOfWords = 0
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentWordsQuizBinding.inflate(inflater, container, false)
-
+        moveToSection = binding.moveToSections
+        moveToSection.setOnClickListener {
+            val action = WordsQuizFragmentDirections.
+            WordsQuizFragmentToSectionsFragment(args.planetId)
+            Navigation.findNavController(binding.root)
+                .navigate(action)
+        }
         val list = args.options.split(",")
-
         binding.questionTextView.text = args.question
 
         for (i in list.indices){
@@ -37,12 +47,12 @@ class WordsQuizFragment : Fragment() {
             questionTextView.layoutParams = ViewGroup.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT)
-            questionTextView.layoutParams.width = 150
+            questionTextView.layoutParams.width = 170
             questionTextView.layoutParams.height = 120
             questionTextView.gravity = Gravity.CENTER
             questionTextView.text = list[i]
             questionTextView.setTextColor(Color.WHITE)
-            questionTextView.textSize = 18F
+            questionTextView.textSize = 16F
             binding.questionLinearLayout.addView(questionTextView)
             questionTextView.background = resources.getDrawable(R.drawable.word_layout)
             listOfButton.add(i,questionTextView)
@@ -54,24 +64,32 @@ class WordsQuizFragment : Fragment() {
                 if(listOfButton[i].parent == binding.questionLinearLayout){
                     owner.removeView(listOfButton[i])
                     binding.answerLinearLayout.addView(listOfButton[i])
+                    countNumOfWords += 1
+                    if(countNumOfWords == list.size){
+                        var sentence = ""
+                        binding.answerLinearLayout.forEach {
+                            val view = it as TextView
+                            sentence += ","+view.text
+                        }
+                        if(sentence.equals(args.answer)){
+                            controlSound(R.raw.correct_sound_effect)
+                        }else{
+                            controlSound(R.raw.incorrect_sound_effect)
+                            for(j in listOfButton.indices){
+                                if(listOfButton[j].parent == binding.answerLinearLayout){
+                                    binding.answerLinearLayout.removeView(listOfButton[j])
+                                    binding.questionLinearLayout.addView(listOfButton[j])
+                                }
+                            }
+
+                            countNumOfWords = 0
+                        }
+                    }
                 }else{
                     owner.removeView(listOfButton[i])
                     binding.questionLinearLayout.addView(listOfButton[i])
+                    countNumOfWords -=1
                 }
-
-            }
-        }
-
-        binding.doneButton.setOnClickListener {
-            var sentence = ""
-            binding.answerLinearLayout.forEach {
-                val view = it as TextView
-                sentence += ","+view.text
-            }
-            if(sentence.equals(args.answer)){
-                Toast.makeText(requireContext(),"correct", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(requireContext(),"wrong", Toast.LENGTH_SHORT).show()
 
             }
         }
@@ -79,5 +97,9 @@ class WordsQuizFragment : Fragment() {
         return binding.root
     }
 
+    private fun controlSound(soundId:Int) {
+        mediaPlayer = MediaPlayer.create(requireContext(), soundId)
+        mediaPlayer.start()
+    }
 
-}
+    }
