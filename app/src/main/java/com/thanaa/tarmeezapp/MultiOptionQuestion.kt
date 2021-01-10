bef90ccf41b0432fb32105858b18d34279e4bf89
Lamia Ewais <lamia5098@gmail.com>
2021-01-10 16:06:10 +0300

    wrap the layout with ScrollView

--- app/src/main/java/com/thanaa/tarmeezapp/MultiOptionQuestion.kt
+++ app/src/main/java/com/thanaa/tarmeezapp/MultiOptionQuestion.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
